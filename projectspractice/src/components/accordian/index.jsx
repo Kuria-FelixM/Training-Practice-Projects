51e3361d075600e3a1b6b21158c719e697c20d1e
Kuria-FelixM <muhiafelix35@gmail.com>
2025-01-06 16:49:41 +0300

    accordian

--- projectspractice/src/components/accordian/index.jsx
+++ projectspractice/src/components/accordian/index.jsx
@@ -4,16 +4,27 @@ import data from "./data";
 export default function Accordian(){
    
    const [selected,setSelected] = useState(null);
+
+   function handleSingleSelection(grtCurrentId){
+    console.log(grtCurrentId);
+    setSelected(grtCurrentId === selected ? null : grtCurrentId);
+
+   }
    
     return <div className="wrapper">
         <div className="accordian">
             {
                 data && data.length > 0 ? 
                 data.map(dataItem=> <div className="item">
-                    <div className="title">
+                    <div onClick={()=>handleSingleSelection(dataItem.id)}className="title">
                         <h3>{dataItem.question}</h3>
                         <span>+</span>
                     </div>
+                    {
+                        selected === dataItem.id ?
+                        <div className="content">{dataItem.answer}</div>
+                        : null
+                    }
                 </div>)
                 : <div>No data Presnt!</div>
             }
